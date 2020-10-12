e658ad6f172ffb21a1ef2acdbbd52b52b67ee185
yoann <yo252yo@gmail.com>
2020-10-12 21:18:57 +0200

    for now stupid process of random linear motion

--- script.js
+++ script.js
@@ -16,18 +16,19 @@ function isThere() {
   return Math.abs(x - target_x) + Math.abs(y - target_y) < MOTION_STEP;
 }
 
-function progressToTarget() {
-  if(isThere()) {return;}
+function progressToTarget(callback) {
+  if(isThere()) {return callback();}
   var dx = Math.sign(target_x - x) * MOTION_STEP;
   var dy = Math.sign(target_y - y) * MOTION_STEP;
   x += dx;
   y += dy;
   refresh();
-  setTimeout(progressToTarget, TICK);
+  setTimeout(function(){progressToTarget(callback);}, TICK);
 }
 
 
-function goTo(x_, y_, time) {
+function goTo(x_, y_, time, callback) {
+  console.log(`Going to: ${x_}, ${y_} in ${time} ms`);
   target_x = x_;
   target_y = y_;
   var d = Math.sqrt(Math.pow(target_x-x,2)+Math.pow(target_y-y,2));
@@ -35,13 +36,26 @@ function goTo(x_, y_, time) {
   if(TICK == MIN_TICK){
     MOTION_STEP = d/(time/MIN_TICK);
   }
-  progressToTarget();
+  progressToTarget(callback);
+}
+
+function linearMove() {
+  var target_x = (2 * Math.random() - 0.5) * window.innerWidth;
+  var target_y = (2 * Math.random() - 0.5) * window.innerHeight;
+  var wait = Math.random() * 2000;
+  var speed = Math.random() * 2000;
+
+  var f = function(){
+    console.log(`Waiting ${wait} ms`);
+    setTimeout(linearMove, wait);
+  }
+  goTo(target_x, target_y, speed, f);
 }
 
 
 
 function move() {
-  goTo(500, 500, 10000);
+  linearMove();
 }
 
 
